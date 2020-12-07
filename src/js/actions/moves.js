06ebea034b2a9a356aaa127e5ce041df7a06feb4
Ricardo Salas <ricardevelopment@gmail.com>
2020-12-07 23:10:29 +0100

    set function piece moves

--- src/js/actions/moves.js
+++ src/js/actions/moves.js
@@ -2,7 +2,7 @@ const moves = {
     '+': (currCoords, destCoords, limit) => {
         const xDif = destCoords[0] - currCoords[0];
         const yDif = destCoords[1] - currCoords[1];
-        console.log(xDif + '--' + yDif + '--' + limit);
+
         if (xDif > 0 && yDif > 0) return currCoords
 
         if (xDif > limit || yDif > limit) return currCoords;
@@ -23,6 +23,7 @@ const moves = {
         const xDif = destCoords[0] - currCoords[0];
         const yDif = destCoords[1] - currCoords[1];
 
+        if (xDif > 0 || yDif > limit) return currCoords
         if (xDif > limit || yDif > limit) return currCoords;
 
         return destCoords;
@@ -31,6 +32,8 @@ const moves = {
     'x': (currCoords, destCoords, limit) => {
         const xDif = destCoords[0] - currCoords[0];
         const yDif = destCoords[1] - currCoords[1];
+
+        if (xDif === 0 && yDif === 0) return currCoords
 
         if (xDif > limit || yDif > limit) return currCoords;
 
@@ -41,7 +44,9 @@ const moves = {
         const xDif = destCoords[0] - currCoords[0];
         const yDif = destCoords[1] - currCoords[1];
 
-        if (xDif > limit || yDif > limit) return currCoords;
+        if (xDif + yDif > limit) return currCoords;
+
+        if((xDif !== 2 && yDif !== 1) || (xDif !== 1 && yDif !== 2)) return currCoords
 
         return destCoords;
     }
