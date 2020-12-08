c88a4c423058cc2e39b129614e9fde58eedbccf3
Ricardo Salas <ricardevelopment@gmail.com>
2020-12-08 18:23:09 +0100

    movePieces change state of pieces and table

--- src/js/actions/actions.js
+++ src/js/actions/actions.js
@@ -1,6 +1,5 @@
 import { createPiece } from '../pieces/index.js'
 import { piecesInfo } from '../pieces/info.js'
-import { moves } from './moves.js'
 
 const actions = {
 
@@ -56,15 +55,150 @@ const actions = {
             table.setCellState(piece, coords)
         }
     },
-
-    movePiece: (table, piece, move) => {
-        const { currCoords, desCoords} = move; 
-        const newPosition = moves[piece.moves](currCoords, desCoords, piece.limitCells)
-        console.log(newPosition)
-        piece.setCurrentCoords(newPosition);
-        table.setCellState('empty', currCoords);
-        table.setCellState(piece, newPosition);
-    }
 }
+
+/* renders table html code due to a tableState */
+window.renderTable = (tableState) => {
+    console.log(tableState);
+    console.log('renderizando')
+    let image;
+    let cells = '';
+    let pieceImage;
+
+    for (let i = 7; i >= 0; i--) {
+        for (let j = 0; j < 8; j++) {
+            const color = tableState[j][i].color;
+            let state = tableState[j][i].state;
+            const coords = tableState[j][i].coords;
+            // console.log(state);
+            // console.log(color);
+            if ( color === 'white') {
+                image = "whiteSquare.png";
+            } else if (color === 'black') {
+                image = "blackSquare.png";
+            }
+
+            let width;
+            let height;
+            let pieceColor;
+            let draggable;
+            let pieceCode;
+
+            if (state.type) {
+                pieceColor = state.color;
+                pieceCode = pieceColor + state.numPiece;
+                state = state.type;
+                width = 25;
+                height = 60;
+                pieceImage =`/pieces/${state}${pieceColor}.png`;
+                draggable = "true";
+            } else {
+                width = 65;
+                height = 65;
+                pieceImage = image;
+                draggable = "false";
+            }
+
+            const cell = 
+            `<span id="${coords}" class="cell dropzone" ondragover="onDragOver(event);" ondrop="onDrop(event);">
+                    <img id="${pieceCode}" ondragstart="onDragStart(event)"; class="piece draggable" draggable=${draggable} src="src/img/${pieceImage}" alt=${state} width="${width}" height="${height}">
+                    <img draggable="false" src="src/img/${image}" alt=${coords} width="65" height="65">
+            </span>`;
+            cells = cells + cell;
+        }
+    }
+
+    const htmlCode = 
+    `<div class="table">
+        ${cells}
+    </div>`;
+    document.getElementById('chessTable').innerHTML = htmlCode;
+};
+
+/* changes state of table and piece */
+window.movePiece = (options) => {
+    const { table, pieces, piece, newCoords } = options;
+    const oldX = piece.currentCoords[0];
+    const oldY = piece.currentCoords[1];
+    const newX = newCoords[0];
+    const newY = newCoords[1];
+    const color = piece.color;
+    const numPiece = piece.numPiece
+    // update piece in pieces with new current coords
+    pieces[color][numPiece]['currentCoords'] = newCoords;
+    // update table new coords cell with state the updated piece
+    table[newX][newY]['state'] = pieces[color][numPiece];
+    // update table old coords cell with state empty
+    table[oldX][oldY]['state'] = 'empty';
+
+    return { table, pieces };
+};
+
+/* check correct moves for every piece type */
+window.checkMoves = (options) => {
+    const { originCoords, currCoords, destCoords, limit, moves } = options;
+    const orgX = originCoords[0];
+    const orgY = originCoords[1];
+    const currX = currCoords[0];
+    const currY = currCoords[1];
+    const destX = destCoords[0];
+    const destY = destCoords[1];
+    const xDif = destX - currX;
+    const yDif = destY - currY;
+    const straigthDist = Math.sqrt(xDif * xDif + yDif * yDif);
+    const cos45 = 0.707106;
+    const diagonalDist = Math.ceil(straigthDist * cos45);
+
+    switch (moves) {
+        case '+': 
+            if (destX !== currX && destY !== currY ) return currCoords;
+    
+            if (straigthDist > limit) return currCoords;
+    
+            return destCoords;
+       
+        case '*':
+            if (destX !== currX && destY !== currY) {  
+                if (diagonalDist > limit) return currCoords;
+
+                if (yDif/xDif !== 1 && yDif/xDif !== -1) return currCoords
+            } else if (destX === currX || destY === currY) {
+                if (straigthDist > limit) return currCoords;
+            } 
+    
+            return destCoords;
+
+        case '|':
+            if (destX !== currX) return currCoords
+
+            if (straigthDist > limit) return currCoords;
+
+            if(orgX === 6 && orgX < destX) return currCoords;
+            
+            if(orgX === 1 && orgX > destX) return currCoords;
+    
+            return destCoords;
+
+        case 'x':
+            if (destX === currX || destY === currY) return currCoords;
+
+            if (diagonalDist > limit) return currCoords;
+
+            if (yDif/xDif !== 1 && yDif/xDif !== -1) return currCoords
+
+            return destCoords;
+
+        case 'L':
+            if (destX === currX || destY === currY) return currCoords;
+            //if (xDif + yDif > limit) return currCoords;
+    
+            if((xDif !== 2 && yDif !== 1) || (xDif !== 1 && yDif !== 2)) return currCoords
+
+            if (yDif/xDif !== 1 && yDif/xDif !== -1) return currCoords
+    
+            return destCoords;
+    }
+};
+
 
 export { actions };
--- src/js/game.js
+++ src/js/game.js
@@ -10,82 +10,9 @@ const startGame = () => {
     pieces.white = actions.createPieces('white');
     pieces.black = actions.createPieces('black');
     actions.setPieces(table, pieces);
-    localStorage.setItem('tableState', JSON.stringify(tableState));
+    localStorage.setItem('table', JSON.stringify(tableState));
     localStorage.setItem('pieces', JSON.stringify(pieces));
-    renderTable(tableState);
-  
-
-
-
-    // const currCoords = whitePieces['king'].getPieceInfo().currentCoords;
-    // const piece = whitePieces['king'];
-    // const move = {
-    //     desCoords: [1, 4],
-    //     currCoords,
-    // }
-    // actions.movePiece(table, piece, move);
-    // console.log(piece);
-    // console.log(table);
-}
-    
-const renderTable = (tableState) => {
-    let image;
-    let cells = '';
-    let pieceImage;
-
-    for (let i = 7; i >= 0; i--) {
-        for (let j = 0; j < 8; j++) {
-            const color = tableState[j][i].color;
-            let state = tableState[j][i].state;
-            const coords = [j, i]
-        
-            if ( color === 'white') {
-                image = "whiteSquare.png";
-            } else if (color === 'black') {
-                image = "blackSquare.png";
-            }
-
-            let width;
-            let height;
-            let pieceColor;
-            let draggable;
-            let numPiece;
-
-            if (state.type) {
-                pieceColor = state.color;
-                numPiece = state.numPiece;
-                state = state.type;
-                width = 25;
-                height = 60;
-                pieceImage =`/pieces/${state}${pieceColor}.png`;
-                draggable = "true";
-            } else {
-                width = 65;
-                height = 65;
-                pieceImage = image;
-                draggable = "false";
-            }
-
-            const cell = 
-            `<span id="${coords}" class="cell dropzone" ondragover="onDragOver(event);" ondrop="onDrop(event);">
-                    <img id="${numPiece}" ondragstart="onDragStart(event)"; class="piece draggable" draggable=${draggable} src="src/img/${pieceImage}" alt=${state} width="${width}" height="${height}">
-                    <img draggable="false" src="src/img/${image}" alt=${coords} width="65" height="65">
-            </span>`;
-            cells = cells + cell;
-        }
-    }
-
-    const htmlCode = 
-    `<div class="table">
-        ${cells}
-    </div>`;
-    document.getElementById('chessTable').innerHTML = htmlCode;
+    window.renderTable(tableState);
 }
 
-
-// const movePiece = (piece, originCoords, currCoords, destCoords) => {
-//     const piece = document.querySelector(`.${piece.state.numPiece}`);
-//     const cells = querySelectorAll('.dropzone')
-// }
-
-export { startGame, renderTable };
+export { startGame };
