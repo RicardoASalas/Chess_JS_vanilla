19c6067d98dfb07250065a58a5788e314c161a77
Ricardo Salas <ricardevelopment@gmail.com>
2020-12-08 18:21:48 +0100

    rerender table after a change dropping a piece

--- src/js/dragAndDrop.js
+++ src/js/dragAndDrop.js
@@ -43,7 +43,22 @@ window.onDrop = (event) => {
     }
 
     const newCoords = checkMoves(options);
-    console.log('newCoords: '+newCoords)
+
+    if(newCoords !== options.currCoords) {
+        let pieces = localStorage.getItem('pieces');
+        pieces = JSON.parse(pieces);
+        let table = localStorage.getItem('table');
+        table = JSON.parse(table);
+        const newState = window.movePiece({table, pieces, piece, newCoords});
+        table = JSON.stringify(newState.table);
+        pieces = JSON.stringify(newState.pieces);
+        localStorage.removeItem(table);
+        localStorage.setItem('table', table);
+        localStorage.removeItem(pieces);
+        localStorage.setItem('pieces', pieces);
+        window.renderTable(newState.table);
+    }
+    
 
     // const draggableElement = document.getElementById(id);
     // const dropzone = event.target;
@@ -55,68 +70,3 @@ window.onDrop = (event) => {
     // .clearData();
 };
 
-/* check correct moves for every piece type */
-window.checkMoves = (options) => {
-    const { originCoords, currCoords, destCoords, limit, moves } = options;
-    const orgX = originCoords[0];
-    const orgY = originCoords[1];
-    const currX = currCoords[0];
-    const currY = currCoords[1];
-    const destX = destCoords[0];
-    const destY = destCoords[1];
-    const xDif = destX - currX;
-    const yDif = destY - currY;
-    const straigthDist = Math.sqrt(xDif * xDif + yDif * yDif);
-    const cos45 = 0.707106;
-    const diagonalDist = Math.ceil(straigthDist * cos45);
-
-    switch (moves) {
-        case '+': 
-            if (destX !== currX && destY !== currY ) return currCoords;
-    
-            if (straigthDist > limit) return currCoords;
-    
-            return destCoords;
-       
-        case '*':
-            if (destX !== currX && destY !== currY) {  
-                if (diagonalDist > limit) return currCoords;
-
-                if (yDif/xDif !== 1 && yDif/xDif !== -1) return currCoords
-            } else if (destX === currX || destY === currY) {
-                if (straigthDist > limit) return currCoords;
-            } 
-    
-            return destCoords;
-
-        case '|':
-            if (destX !== currX) return currCoords
-
-            if (straigthDist > limit) return currCoords;
-
-            if(orgX === 6 && orgX < destX) return currCoords;
-            
-            if(orgX === 1 && orgX > destX) return currCoords;
-    
-            return destCoords;
-
-        case 'x':
-            if (destX === currX || destY === currY) return currCoords;
-
-            if (diagonalDist > limit) return currCoords;
-
-            if (yDif/xDif !== 1 && yDif/xDif !== -1) return currCoords
-
-            return destCoords;
-
-        case 'L':
-            if (destX === currX || destY === currY) return currCoords;
-            //if (xDif + yDif > limit) return currCoords;
-    
-            if((xDif !== 2 && yDif !== 1) || (xDif !== 1 && yDif !== 2)) return currCoords
-
-            if (yDif/xDif !== 1 && yDif/xDif !== -1) return currCoords
-    
-            return destCoords;
-    }
-};
