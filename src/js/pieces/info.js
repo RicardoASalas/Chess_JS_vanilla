c05db3255ec1b54fb09658de0e447def1a18e09b
Ricardo Salas <ricardevelopment@gmail.com>
2020-12-08 14:54:16 +0100

    refactor coordinates as they were turned

--- src/js/pieces/info.js
+++ src/js/pieces/info.js
@@ -3,48 +3,48 @@ const piecesInfo = [
         name: 'queen',
         numPieces: 1,
         originCoords: {
-            white: [[0, 3]],
-            black: [[7, 3]]
+            white: [[3, 0]],
+            black: [[3, 7]]
         }
     },
     {
         name: 'king',
         numPieces: 1,
         originCoords: {
-            white: [[0, 4]],
-            black: [[7, 4]]
+            white: [[4, 0]],
+            black: [[4, 7]]
         }
     },
     {
         name: 'bishop',
         numPieces: 2,
         originCoords: {
-            white: [[0, 2], [0, 5]],
-            black: [[7, 2], [7, 5]]
+            white: [[2, 0], [5, 0]],
+            black: [[2, 7], [5, 7]]
         }
     },
     {
         name: 'knight',
         numPieces: 2,
         originCoords: {
-            white: [[0, 1], [0, 6]],
-            black: [[7, 1], [7, 6]]
+            white: [[1, 0], [6, 0]],
+            black: [[1, 7], [6, 7]]
         }
     },
     {
         name: 'castle',
         numPieces: 2,
         originCoords: {
-            white: [[0, 0], [0, 7]],
-            black: [[7, 0], [7, 7]]
+            white: [[0, 0], [7, 0]],
+            black: [[0, 7], [7, 7]]
         }
     },
     {
         name: 'pawn',
         numPieces: 8,
         originCoords: {
-            white: [[1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]],
-            black: [[6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7]]
+            white: [[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1]],
+            black: [[0, 6], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6], [7, 6]]
         }
     }
 ];
